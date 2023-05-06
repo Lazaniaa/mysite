4b49527205fb7586868e0eb4a9f84856f15f66c3
Lazaniaa <valera2922@gmail.com>
2023-05-05 23:20:23 -0700

    final

--- main/static/main/scripts/script.js
+++ main/static/main/scripts/script.js
@@ -1,3 +1,17 @@
-AOS.init({ duration: 1500,  });
+AOS.init({ duration: 1500, disable: 'mobile' });
 
 
+function backToTop()
+{
+    let button = $('.link-top');
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 500)
+        {
+            button.fadeIn();
+        }else{
+            button.fadeOut();
+        }
+    });
+
+}
+backToTop();
